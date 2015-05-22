1802db09f367a283174c15de426033376bd81d60
Devin Mui <devinmui@yahoo.com>
2015-05-22 05:01:15 +0000

    setups

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'rapiflash',
+  packages = ['rapiflash'],
+  version = '0.1',
+  description = 'A Python wrapper for Rapiflash (Real-time Alerts of Potential Increases to Flood Level for Avoiding Significant Harm) which allows developers to gather river water levels from around the world',
+  author = 'Devin Mui',
+  author_email = 'devinmui@yahoo.com',
+  url = 'https://github.com/devinmui/rapiflash-api',
+  download_url = 'https://github.com/devinmui/rapiflash-python/tarball/0.1',
+  keywords = ['floods'],
+)
